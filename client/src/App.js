9e537504db80b12c5288443e1189a6326b3b9a12
gajjardarshithasmukhbhai <darshit.hasmukhbhai1998@gmail.com>
2023-12-02 19:40:27 +0530

    Feat(): Docker Kubernetes code Updated

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,8 @@ import PostList from "./PostList";
 
 const App = () => {
   return (
-    <div className="container ">
-      <br />
-      <h1>Darshit Node JS Distributed Small App</h1>
+    <div className="container">
+      <h1>Create Post</h1>
       <PostCreate />
       <hr />
       <h1>Posts</h1>
